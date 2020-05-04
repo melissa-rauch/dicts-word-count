6e4e3d0a1a3222f7816309d75c3854142de889b2
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-04 22:55:26 +0000

    Completed base version of function word_count

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,21 @@
-# put your code here.
+
+def word_count(file_name):
+    """Returns each word in given text and the number of times the word appears"""
+    given_text = open(file_name)
+    word_list = {}
+
+    for line in given_text:
+        words = line.rstrip().split()
+        
+        for word in words:
+            word_list[word] = word_list.get(word, 0) + 1
+    
+    # return word_list               
+
+    for key,value in word_list.items():
+        
+        print (f"{key} {value}")
+
+
+
+word_count("twain.txt")  
